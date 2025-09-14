0df7423ea51fbc956cc5bad43e2f2c460de9d442
Archmage16 <mken9195@gmail.com>
2025-09-14 21:06:04 +0500

    just books

--- BookSales/urls.py
+++ BookSales/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from .views import IndexView
+
+app_name = 'BookSales'
+
 
 urlpatterns = [
-    
+    path('index', IndexView.as_view(), name='index'),
 ]
--- BookSales/views.py
+++ BookSales/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Book, Author, Publisher
 # Create your views here.
 class IndexView(TemplateView):
-    template_name = 'ReloadBook.html'
+    template_name = 'bookSales/mainBooks.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-
+        context['Books'] = Book.objects.all()
         
         return context
     
