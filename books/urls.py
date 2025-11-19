76a24ab4089117e70f153f846de1f34850dbf6b3
Archadmin <mken9195@gmail.com>
2025-11-19 19:53:50 +0500

    Cookies homework

--- books/urls.py
+++ books/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('search/', views.search_books, name='search'),
+    path('books/', views.book_list, name='book_list'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from .models import Book
 from .forms import SearchForm
-
+from datetime import datetime
 def home(request):
-    return render(request, 'home.html')
+    username = request.COOKIES.get('username', 'Неизвестный пользователь')
+    return render(request, 'home.html', {'username': username})
+
+def book_list(req):
+    books = Book.objects.all()
+
+    last_visit = req.COOKIES.get('last_visit', 'Первый раз?')
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    visits = int(req.COOKIES.get('visits', 0))
+    visits+=1
+    response = render(req, 'book_list.html', {'books': books, 
+                                                  'visits': visits,
+                                                  'last_visit': last_visit})
+    response.set_cookie('last_visit', now, max_age=60*60*24*30)
+    
+    response.set_cookie('visits', visits, max_age=3600*24*30)
+    return response
+
 
 def search_books(request):
     form = SearchForm(request.GET)
