6c6bf81546902a78a0cfee818ee4fd52a39f8525
Archadmin <mken9195@gmail.com>
2025-11-19 20:04:19 +0500

    messages in Django

--- books/views.py
+++ books/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Book
 from .forms import SearchForm
 from datetime import datetime
+from django.contrib import messages
+from django.core import signing
+
 def home(request):
     username = request.COOKIES.get('username', 'Неизвестный пользователь')
     return render(request, 'home.html', {'username': username})
@@ -11,15 +14,22 @@ def book_list(req):
 
     last_visit = req.COOKIES.get('last_visit', 'Первый раз?')
     now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    messages.info(req, f"Ваш предыдущий визит: {last_visit}")
 
-    visits = int(req.COOKIES.get('visits', 0))
-    visits+=1
-    response = render(req, 'book_list.html', {'books': books, 
-                                                  'visits': visits,
-                                                  'last_visit': last_visit})
-    response.set_cookie('last_visit', now, max_age=60*60*24*30)
+
+
+    visits = int(req.COOKIES.get('visits', 0))+1
+    signed_visits = signing.dumps({"visits": visits})
+
+    context = {'books': books, 
+               'visits': visits,
+               'last_visit': last_visit,
+               'signed_visits': signed_visits,}
+    response = render(req, 'book_list.html',context=context)
     
+    response.set_cookie('last_visit', now, max_age=60*60*24*30)
     response.set_cookie('visits', visits, max_age=3600*24*30)
+
     return response
 
 
